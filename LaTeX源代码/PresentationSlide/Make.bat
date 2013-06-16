9ef2b6e9eabe4bbc358ae23468e2efd996df524a
CUG-Onion <cug_yangyuancong@hotmail.com>
2013-06-16 16:13:38 +0800

    Make.bat 脚本的备份部分添加操作系统版本判断 windows7 和windows xp 的桌面路径不一样xp 为桌面,Windows7为Desktop

--- LaTeX源代码/PresentationSlide/Make.bat
+++ LaTeX源代码/PresentationSlide/Make.bat
@@ -2,8 +2,14 @@
 ::删除原pdf文件同时作备份源文件到当前用户的桌面,随时备份文件是很重要的
 ::参看http://blog.csdn.net/wzsbll/article/details/6690895
 del *.pdf
-::del /Q /S "%HOMEDRIVE%%HOMEPATH%\Desktop\PresentationSlide\*"
-XCOPY  "*"  "%HOMEDRIVE%%HOMEPATH%\Desktop\PresentationSlide" /E /Y /D
+::判断系统版本 只判断XP与Win7,根据返回值判断 http://blog.163.com/xqslove@yeah/blog/static/1670140142012112221429906/  http://www.myexception.cn/operating-system/726032.html
+@ver|findstr "5.1"
+::如果是Windows7 目标设为 Desktop
+@if %errorlevel% equ 1 (set Dest=Desktop)
+::如果是Windows XP  目标设为 桌面
+@if %errorlevel% equ 0 (set Dest=桌面)
+::del  /Q /S  "%HOMEDRIVE%%HOMEPATH%\%Dest%\PresentationSlide"
+XCOPY  "*"  "%HOMEDRIVE%%HOMEPATH%\%Dest%\PresentationSlide" /E /Y /D
 @pause
 ::exit
 
@@ -17,9 +23,9 @@ makeindex -o "%Name%.ind" "%Name%.idx"
 Config\GBK2Uni_Windows.exe "%Name%.out"
 pdflatex "%Name%.tex"
 
-rename %Name%.pdf  光切法显微镜探究荷叶效应__演示文稿__杨远聪.pdf
-::start %Name%.pdf
-start	光切法显微镜探究荷叶效应__演示文稿__杨远聪.pdf
+::打开文件
+start %Name%.pdf
+
 
 Config\Cleaner.bat
 @pause
